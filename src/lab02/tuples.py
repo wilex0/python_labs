65acd5587af10ff8b5b96ecacdd82f0df37c6051
wilex <143896248+wilex0@users.noreply.github.com>
2025-09-22 23:51:53 +0300

    buuu

--- src/lab02/tuples.py
+++ src/lab02/tuples.py
@@ -1,9 +1,9 @@
 def format_record(rec: tuple[str, str, float]) -> str:
     if len(rec) != 3 or not (isinstance(rec[0], str) and isinstance(rec[1], str)):
         raise ValueError
-    if not isinstance(rec[2], float):
+    if not isinstance(rec[2], (int, float)) or not ( 1 < rec[2] < 5 ):
         raise TypeError
-    if not (len(rec[0].strip().split()) in [2,3]) or not (rec[1].find('-') != -1 and all(len(x) > 0 for x in rec[1].strip().split('-'))):
+    if not (2 <= len(rec[0].strip().split()) <= 3) or not (rec[1].find('-') != -1 and all(len(x) > 0 for x in rec[1].strip().split('-'))):
         raise ValueError
 
     inits = [i[0].upper() + i[1:] for i in rec[0].strip().split()]
