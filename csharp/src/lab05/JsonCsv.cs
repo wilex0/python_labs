12a88765cfdc16c299d84a6a97dffaf4b098fdd1
wilex <143896248+wilex0@users.noreply.github.com>
2025-10-28 07:25:43 +0300

    buuu

---
+++ csharp/src/lab05/JsonCsv.cs
@@ -0,0 +1,87 @@
+﻿using System.Text.Json;
+using Lib;
+namespace System.Text;
+public static partial class CsvXlsx
+{
+    public static ValueTuple<string?, string?> CheckPath(string? readPath, string? writePath)
+    {
+        if (!File.Exists(readPath))
+            if (!File.Exists(Lib.Text.ProjectDirectory + readPath))
+                throw new ArgumentException($"File {readPath} does not exist");
+            else 
+                readPath = Lib.Text.ProjectDirectory;
+        if (!Directory.Exists(Path.GetDirectoryName(writePath)))
+            if (!Directory.Exists(Path.GetDirectoryName(Lib.Text.ProjectDirectory + writePath)))
+                throw new ArgumentException($"Directory {Path.GetDirectoryName(writePath)} does not exist");
+            else 
+                writePath = Lib.Text.ProjectDirectory + writePath;
+        return (readPath, writePath);
+    }
+    public static string? CheckPath(string? inputPath)
+    {
+        if (!File.Exists(inputPath))
+            if (!File.Exists(Lib.Text.ProjectDirectory + inputPath))
+                throw new FileNotFoundException($"Файл {inputPath} не найден");
+            else
+                inputPath = Lib.Text.ProjectDirectory + inputPath;
+        return inputPath;
+    }
+    public static void CheckPath(ref string? inputPath)
+    {
+        if (!File.Exists(inputPath))
+            if (!File.Exists(Lib.Text.ProjectDirectory + inputPath))
+                throw new FileNotFoundException($"Файл {inputPath} не найден");
+            else
+                inputPath = Lib.Text.ProjectDirectory + inputPath;
+    }
+    public static void JsonToCsv(string? jsonPath, string? csvPath)
+    {
+        (jsonPath, csvPath) = CheckPath(jsonPath, csvPath);
+        using StreamReader fs = new StreamReader(jsonPath, Encoding.UTF8);
+        if (fs.EndOfStream)
+            throw new ArgumentException($"File {jsonPath} is empty");
+        string jsonContent = fs.ReadToEnd();
+        using (JsonDocument doc = JsonDocument.Parse(jsonContent))
+        {
+            JsonElement root = doc.RootElement;
+            if (root.GetArrayLength() == 0)
+                throw new JsonException("Root element length is zero.");
+            var items = root.EnumerateArray();
+            
+            var header = root[0].EnumerateObject().Select(x => x.Name);
+            
+            if (items.Any(x => x.EnumerateObject().Count() != header.Count()) ||
+                items.Any(x => !x.EnumerateObject().Select(x => x.Name).SequenceEqual(header)))
+            {
+                throw new ArgumentException("Uncorrected keys");
+            }
+            
+            using StreamWriter writer = new StreamWriter(csvPath, false, Encoding.UTF8);
+            writer.WriteLine(string.Join(',', header));
+            foreach (var el in items)
+                writer.WriteLine(string.Join(',' , el.EnumerateObject().Select(x => x.Value)));
+        }
+    }
+
+    public static void CsvToJson(string? csvPath, string? jsonPath)
+    {
+        (csvPath, jsonPath) = CheckPath(csvPath, jsonPath);
+        using StreamReader fs = new StreamReader(csvPath, Encoding.UTF8);
+        if (fs.EndOfStream)
+            throw new ArgumentException($"File {csvPath} is empty");
+        var header = fs.ReadLine().Split(',');
+        
+        List<Dictionary<string, string>> items = new();
+        while (!fs.EndOfStream)
+        {
+            var line = fs.ReadLine().Split(',');
+            if (line.Length != header.Length)
+                throw new ArgumentException($"Invalid number of lines in file {csvPath}");
+            items.Add(header.Zip(line, (x, y) => new { x, y }).ToDictionary(x => x.x, x => x.y ));
+        }
+        
+        var result = JsonSerializer.Serialize(items);
+        using StreamWriter writer = new StreamWriter(jsonPath, false, Encoding.UTF8);
+        writer.Write(result);
+    }
+}
---
+++ csharp/src/lab06/CLI.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.CommandLine;
+using Lib;
+namespace Lab06;
+class CLI
+{
+    public static int Main(string[] args)
+    {
+        Option<string> inputFileOp = new("--in") { Description = "Путь к читаемому файлу (--in <string>)" };
+        Option<string> outputFileOp = new("--out") { Description = "Путь к файлу для записи результата (--out <string>)"};
+        Option<int> topOp = new("--top") { Description = "Вывод необходимого количества элементов (--top <int>)"};
+        Option<bool> countOp = new("-n") { Description = "Флаг для нумерации строк (-n/without -n)"};
+        Option<bool> tableOp = new("-table") { Description = "Флаг для удобного табличного вывода (-table/without -table)" };
+
+        Command csv2jsonCommand = new Command("csv2json", "csv в json") { inputFileOp, outputFileOp };
+        Command json2csvCommand = new Command("json2csv", "json в csv") { inputFileOp, outputFileOp };
+        Command csv2xlsxCommand = new Command("csv2xlsx", "csv в xlsx") { inputFileOp, outputFileOp };
+        
+        csv2jsonCommand.SetAction(parseRes =>
+        {
+            var (input, output) = (parseRes.GetValue(inputFileOp), parseRes.GetValue(outputFileOp));
+            (input, output) = CsvXlsx.CheckPath(input, output);
+            CsvXlsx.CsvToJson(input, output);
+        }); 
+        json2csvCommand.SetAction(parseRes =>
+        {
+            var (input, output) = (parseRes.GetValue(inputFileOp), parseRes.GetValue(outputFileOp));
+            (input, output) = CsvXlsx.CheckPath(input, output);
+            CsvXlsx.JsonToCsv(input, output);
+        });
+        csv2xlsxCommand.SetAction(parseRes =>
+        {
+            var (input, output) = (parseRes.GetValue(inputFileOp), parseRes.GetValue(outputFileOp));
+            (input, output) = CsvXlsx.CheckPath(input, output);
+            CsvXlsx.CsvToXlsx(input, output);
+        });
+        
+        Command readCommand = new Command("cat", "Чтение и вывод построчно строк из файла с возможностью нумерации каждой строки") { inputFileOp, countOp };
+        readCommand.SetAction(parseRes =>
+        {
+            var input = parseRes.GetValue(inputFileOp);
+            CsvXlsx.CheckPath(ref input);
+            var isCount = parseRes.GetValue(countOp);
+            using StreamReader reader = new(input);
+            for (int i = 1; !reader.EndOfStream; ++i)
+                Console.WriteLine($"{(isCount ? $"{i}. " : "")}{reader.ReadLine()}");
+        });
+
+        Command statsCommand = new Command("stats", "Вывод частот строк в файле с возможностью необходимого количества элементов + сортировка") { inputFileOp, topOp, tableOp };
+        statsCommand.SetAction(parseRes =>
+        {
+            var input = parseRes.GetValue(inputFileOp);
+            CsvXlsx.CheckPath(ref input);
+            var top = parseRes.GetValue(topOp);
+            var table = parseRes.GetValue(tableOp);
+            var text = IOTxtCsv.ReadText(input, Encoding.UTF8);
+            
+            var dic = Text.CountFreq(Text.Tokenize(Text.Normalize(text)));
+            if (top != 0)
+                dic = Text.TopN(dic, top);
+            if (table)
+                Output.Table(dic);
+            else
+                foreach (var keyValuePair in dic)
+                    Console.WriteLine($"{keyValuePair.Key} {keyValuePair.Value}");
+        });
+        
+        RootCommand rootCommand = new();
+        rootCommand.Subcommands.Add(csv2jsonCommand);
+        rootCommand.Subcommands.Add(json2csvCommand);
+        rootCommand.Subcommands.Add(csv2xlsxCommand);
+        rootCommand.Subcommands.Add(readCommand);
+        rootCommand.Subcommands.Add(statsCommand);
+
+        return rootCommand.Parse(args).Invoke();
+    }
+}
